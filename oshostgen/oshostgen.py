7903c97031137c9c0ff94064a9956ecc52df79ed
jkilpatr <jkilpatr@redhat.com>
2017-06-19 09:57:01 -0400

    fix for newer api and entrypoint

--- oshostgen/oshostgen.py
+++ oshostgen/oshostgen.py
@@ -1,7 +1,7 @@
 import argparse
 import lib.Tools
 import sys
-import openstack
+from openstack import connection
 import os
 
 
@@ -12,45 +12,44 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 
-parser = MyParser(description='A Ansible hostfile generator for \
-                               large OpenStack deployments')
+def main():
+    parser = MyParser(description='A Ansible hostfile generator for \
+                                   large OpenStack deployments')
 
-parser.add_argument('-n', '--no-touch', dest='no_touch', action="store_true",
-                    help="Don't load a key")
+    parser.add_argument('-n', '--no-touch', dest='no_touch',
+                        action="store_true", help="Don't load a key")
 
-parser.add_argument('-k', '--keyfile', dest='keyfile', type=str,
-                    default="$HOME/.ssh/id_rsa", help="Keyfile to load")
+    parser.add_argument('-k', '--keyfile', dest='keyfile', type=str,
+                        default="$HOME/.ssh/id_rsa", help="Keyfile to load")
 
-parser.add_argument('-b', '--browbeat', dest='browbeat',
-                    type=str, default=None,
-                    help="Browbeat mode. Usage: -b <browbeat machine ip>")
+    parser.add_argument('-b', '--browbeat', dest='browbeat',
+                        type=str, default=None,
+                        help="Browbeat mode. Usage: -b <browbeat machine ip>")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if 'OS_AUTH_URL' not in os.environ:
-    print("Please source a cloud rc file")
-    exit(1)
+    if 'OS_AUTH_URL' not in os.environ:
+        print("Please source a cloud rc file")
+        exit(1)
 
+    auth_args = {
+        'auth_url': os.environ['OS_AUTH_URL'],
+        'project_name': 'admin',
+        'username': os.environ['OS_USERNAME'],
+        'password': os.environ['OS_PASSWORD'],
+    }
 
-auth_args = {
-    'auth_url': os.environ['OS_AUTH_URL'],
-    'project_name': 'admin',
-    'username': os.environ['OS_USERNAME'],
-    'password': os.environ['OS_PASSWORD'],
-}
+    conn = connection.Connection(**auth_args)
+    if not lib.Tools.add_key(args.keyfile, conn):
+        print("There are no keys in Nova, please add one")
+        exit(1)
 
+    with open("ssh-config", 'w') as ssh_conf:
+        lines = lib.Tools.build_ssh_config(conn, args)
+        for line in lines:
+            ssh_conf.write(line)
 
-conn = openstack.connection.Connection(**auth_args)
-if not lib.Tools.add_key(args.keyfile, conn):
-    print("There are no keys in Nova, please add one")
-    exit(1)
-
-with open("ssh-config", 'w') as ssh_conf:
-    lines = lib.Tools.build_ssh_config(conn, args)
-    for line in lines:
-        ssh_conf.write(line)
-
-with open("hosts", 'w') as hosts_file:
-    lines = lib.Tools.build_ansible_host_file(conn, args)
-    for line in lines:
-        hosts_file.write(line)
+    with open("hosts", 'w') as hosts_file:
+        lines = lib.Tools.build_ansible_host_file(conn, args)
+        for line in lines:
+            hosts_file.write(line)
